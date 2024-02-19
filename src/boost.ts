22f02d9e98b47d7674504ebbd9e2ffbb9650416f
Scott Piriou <30843220+pscott@users.noreply.github.com>
2024-02-19 16:39:33 +0100

    sepolia chainId

--- src/boost.ts
+++ src/boost.ts
@@ -126,7 +126,7 @@ export function handleMint(event: MintEvent): void {
   }
 
   boostEntity.strategyURI = event.params.strategyURI
-  boostEntity.chainId = "1"
+  boostEntity.chainId = "11155111"
   boostEntity.token = tokenAddress
   boostEntity.poolSize = event.params.boost.balance.toString()
   boostEntity.currentBalance = boostEntity.poolSize;
